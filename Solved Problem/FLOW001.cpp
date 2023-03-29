8ef580aabb3966e8fd3817053ebac36692775953
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-03-30 00:57:55 +0600

    Add files via upload

---
+++ Solved Problem/FLOW001.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int T;
+    cin>>T;
+
+    while(T--){
+        int A,B;
+        cin>>A>>B;
+        int sum = A+B;
+        cout<<sum<<endl;
+    }
+return 0;
+}
